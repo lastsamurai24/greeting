560118a3b436694c6fd130e18da7400b30f335a8
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-25 14:45:23 +0900

    あいさつアプリ課題完了

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+
+    public function morning()
+    {
+        return view('comments.morning');
+    }
+    public function afternoon()
+    {
+        return view('comments.afternoon');
+    }
+    public function evening()
+    {
+        return view('comments.evening');
+    }
+    public function night()
+    {
+        return view('comments.night');
+    }
+    public function freeword($msg)
+    {
+        return view('comments.freeword', ['msg' => $msg]);
+    }
+    public function shuffle()
+    {
+        $greeting = ['おはよう', 'こんにちは', 'こんばんは', 'おやすみ'];
+        shuffle($greeting);
+        foreach ($greeting as $greeting) {
+        }
+        return view('comments.shuffle', ['greeting' => $greeting]);
+    }
+}
